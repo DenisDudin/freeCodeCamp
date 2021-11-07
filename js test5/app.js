ab1faf3221dd55d1492b01de32b9e4329c26db49
Denis Dudin <denis.dudin.a@gmail.com>
2021-11-07 22:49:38 +0300

    Add gallary 2

---
+++ js test5/app.js
@@ -0,0 +1,38 @@
+const upBut = document.querySelector('.up-but');
+const downBut = document.querySelector('.down-but');
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide')
+const slidersCount = mainSlide.querySelectorAll('div').length
+let activeSlide = 0;
+const container = document.querySelector('section')
+const height = container.clientHeight;
+
+
+sidebar.style.top = `-${(slidersCount - 1)*100}vh`;
+
+upBut.addEventListener('click', () => {
+  changeSlide('up');
+})
+
+downBut.addEventListener('click', () => {
+  changeSlide('down');
+})
+
+function changeSlide(direction) {
+  if(direction === 'up') {
+    activeSlide++;
+    if(activeSlide === slidersCount) {
+      activeSlide = 0;
+    }
+  } else if(direction === 'down') {
+    activeSlide--;
+    if(activeSlide < 0) {
+      activeSlide = slidersCount - 1;
+    }
+  }
+
+  
+  mainSlide.style.transform = `translateY(-${activeSlide * height}px)`;
+  sidebar.style.transform = `translateY(${activeSlide * height}px)`
+}
+
